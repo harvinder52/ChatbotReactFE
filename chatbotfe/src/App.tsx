33adc5cb2c1caa9bd29d674a3293e81507babd38
Harv <85876365+harvinder52@users.noreply.github.com>
2023-06-16 13:44:10 +0530

    added page 1

---
+++ chatbotfe/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import EnrollmentForm from "./components/enroll";
+
+function App() {
+  return (
+    <div className="App">
+      <EnrollmentForm />
+    </div>
+  );
+}
+
+export default App;
